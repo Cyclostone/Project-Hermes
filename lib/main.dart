5e7ae89e8686368b622ee3b4f7858537d98c6ace
Arpit Shrotriya <arpit.shrotriya5945@gmail.com>
2021-03-13 07:34:16 +0530

    Github GraphQL Access  Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'package:gql_link/gql_link.dart';
+import 'package:project_hermes_v1/src/github_gql/github_queries.data.gql.dart';
+import 'package:project_hermes_v1/src/github_gql/github_queries.req.gql.dart';
 import 'github_oauth_credentials.dart';
+
 import 'src/github_login.dart';
 
 void main() {
@@ -10,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Github GraphQL API Client',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -28,17 +35,43 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GithubLoginWidget(
         builder: (context, httpClient) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(title),
-            ),
-            body: Center(
-              child: Text('You are Logged in to GitHub!'),
-            ),
+          final link = HttpLink('https://api.github.com/graphql',
+              httpClient: httpClient);
+          return FutureBuilder<$ViewerDetail$viewer>(
+            future: viewerDetail(link), //Completed Future Method
+            builder: (context, snapshot) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(title),
+                ),
+                body: Center(
+                  child: Text(snapshot.hasData
+                      ? 'Hello ${snapshot.data.login}'
+                      : 'Retrieving viewer login details...'),
+                ),
+              );
+            },
           );
         },
         githubClientId: githubClientId,
         githubClientSecret: githubClientSecret,
         githubScopes: githubScopes);
+  }
+}
+
+Future<$ViewerDetail$viewer> viewerDetail(Link link) async {
+  var result = await link.request(ViewerDetail((b) => b)).first;
+  if (result.errors != null && result.errors.isNotEmpty) {
+    throw QueryException(result.errors);
+  }
+  return $ViewerDetail(result.data).viewer;
+}
+
+class QueryException implements Exception {
+  QueryException(this.errors);
+  List<GraphQLError> errors;
+  @override
+  String toString() {
+    return 'Query Excception: ${errors.map((err) => '$err').join(',')}';
   }
 }
---
+++ lib/src/github_gql/github_queries.var.gql.dart
@@ -0,0 +1,5 @@
+//Implementing the library.
+
+class ViewerDetailVarBuilder {
+  final Map<String, dynamic> variables = <String, dynamic>{};
+}
